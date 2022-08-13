5422f62d6356527b88c5e2b407513baf74f42104
Ixf1nity <xa583179@gmail.com>
2022-08-13 20:42:12 +0530

    Fixing

--- src/main/java/me/infinity/groupstats/group/GroupNodeContainer.java
+++ src/main/java/me/infinity/groupstats/group/GroupNodeContainer.java
@@ -3,11 +3,18 @@ package me.infinity.groupstats.group;
 import me.infinity.groupstats.GroupStatsPlugin;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 public class GroupNodeContainer extends HashMap<String, GroupNode> {
 
   public GroupNode getGroupNode(String string) {
-    return this.get(string);
+    Optional<GroupNode> groupNodeOptional = Optional.ofNullable(this.get(string));
+    if (groupNodeOptional.isPresent()) {
+      return groupNodeOptional.get();
+    } else {
+      this.putIfAbsent(string, new GroupNode());
+    }
+    return groupNodeOptional.get();
   }
 
   public String toJson() {
